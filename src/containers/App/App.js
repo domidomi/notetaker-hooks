b9d8c66aaa6fe50bd9b544a077d064907b3e9d6d
domidomi <dpodgorskad@gmail.com>
2019-03-11 16:10:42 +0100

    Base app structure

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import "./App.scss";
 
+import { Container, Row, Col } from "react-bootstrap";
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1> Notetaker app!</h1>
-      </div>
+      <Container fluid={true} className="app-container">
+        <Row>
+          <Col md={4}>Sidebar</Col>
+          <Col>
+            <h1> Notetaker app!</h1>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
