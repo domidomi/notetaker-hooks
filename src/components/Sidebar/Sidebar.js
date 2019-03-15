9f46f9aff1cc9c44d3c9e2af5814f5244b2d86f4
domidomi <dpodgorskad@gmail.com>
2019-03-15 14:46:37 +0100

    base data added, App switched to hooks

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Row } from "react-bootstrap";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <Row>Here is sidebar</Row>
+      <Row>Here is sidebar</Row>
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,21 +1,31 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.scss";
 
 import { Container, Row, Col } from "react-bootstrap";
 
-class App extends Component {
-  render() {
-    return (
-      <Container fluid={true} className="app-container">
-        <Row>
-          <Col md={4}>Sidebar</Col>
-          <Col>
-            <h1> Notetaker app!</h1>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-}
+import { Sidebar } from "../../components";
+
+import notesData from "../../assets/notes.json";
+
+const App = () => {
+  const [notes, setNotes] = useState(null);
+
+  useEffect(() => {
+    
+  });
+
+  return (
+    <Container fluid={true} className="app-container">
+      <Row>
+        <Col md={4}>
+          <Sidebar />
+        </Col>
+        <Col>
+          <h1> Notetaker app!</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default App;
