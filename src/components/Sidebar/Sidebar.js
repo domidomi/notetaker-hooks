a51980c4e00cc8d44d92858f2346ca9a0dc8c7f9
domidomi <dpodgorskad@gmail.com>
2019-03-19 16:41:39 +0100

    Generating notes color cards in the sidebar working; Added styled-components package;

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
-import { Row } from "react-bootstrap";
+import CategoryCard from "../CategoryCard/CategoryCard";
+import tagsData from "../../assets/tags.json";
 
-const Sidebar = () => {
+const SidebarContent = styled.div`
+  background-color: #fff;
+  height: 100%;
+  width: 100%;
+  padding: 20px;
+`;
+
+const Sidebar = ({ notes }) => {
+  const [categories, setCategories] = useState(null);
+
+  useEffect(() => {
+    const cats = tagsData.map(tag => {
+      const matchingNotesIDs = notes
+        .filter(note => note.tags.includes(tag.name))
+        .map(el => el["_id"]);
+
+      return {
+        ...tag,
+        matchingNotesIDs
+      };
+    });
+
+    setCategories(cats);
+  }, [notes]);
+
   return (
-    <div>
-      <Row>Here is sidebar</Row>
-      <Row>Here is sidebar</Row>
-    </div>
+    <SidebarContent>
+      {console.log(notes)}
+      {console.log(categories)}
+
+      {categories &&
+        categories.map(cat => <CategoryCard category={cat} notes={notes} />)}
+
+      {!categories && <h4>No notes :(</h4>}
+    </SidebarContent>
   );
 };
 
