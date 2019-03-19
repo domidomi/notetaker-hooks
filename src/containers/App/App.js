932e6fd3b6ac7087fb932ad1d89161cb0617d540
domidomi <dpodgorskad@gmail.com>
2019-03-19 16:43:34 +0100

    Content base layout created - sidebar and content

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,30 +1,54 @@
 import React, { useState, useEffect } from "react";
 import "./App.scss";
 
-import { Container, Row, Col } from "react-bootstrap";
+import styled from "styled-components";
 
 import { Sidebar } from "../../components";
 
 import notesData from "../../assets/notes.json";
 
-const App = () => {
-  const [notes, setNotes] = useState(null);
+const StyledContainer = styled.div`
+  background-color: #eee;
+  display: flex;
+  height: 100vh;
+`;
 
-  useEffect(() => {
-    
-  });
+const StyledSidebar = styled.div`
+  width: 20%;
+  min-width: 300px;
+  height: 100%;
+  flex: 0 0 auto;
+`;
+
+const StyledContent = styled.div`
+  flex: 1 1 auto;
+  overflow: auto;
+`;
+
+const Title = styled.div`
+  font-size: 160px;
+`;
+
+const App = () => {
+  const [notes, setNotes] = useState(notesData);
+
+  useEffect(() => {});
 
   return (
-    <Container fluid={true} className="app-container">
-      <Row>
-        <Col md={4}>
-          <Sidebar />
-        </Col>
-        <Col>
-          <h1> Notetaker app!</h1>
-        </Col>
-      </Row>
-    </Container>
+    <StyledContainer fluid={true}>
+      <StyledSidebar>
+        <Sidebar notes={notes} />
+      </StyledSidebar>
+      <StyledContent>
+        <Title> Notetaker app!</Title>
+        <Title> Notetaker app!</Title>
+        <Title> Notetaker app!</Title>
+        <Title> Notetaker app!</Title>
+        <Title> Notetaker app!</Title>
+        <Title> Notetaker app!</Title>
+        <Title> Notetaker app!</Title>
+      </StyledContent>
+    </StyledContainer>
   );
 };
 
