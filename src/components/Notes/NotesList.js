0067a4f77628107dd4ac99080f8dd8ff8c9c8b40
domidomi <dpodgorskad@gmail.com>
2019-03-25 13:12:02 +0100

    Added masonry

--- src/components/Notes/NotesList.js
+++ src/components/Notes/NotesList.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Masonry from "react-masonry-component";
 import styled from "styled-components";
 import { NoteCard } from "../";
 
@@ -6,6 +7,11 @@ const NotesListContent = styled.div`
   padding: 20px;
 `;
 
+const masonryOptions = {
+  transitionDuration: 0
+};
+
+const imagesLoadedOptions = { background: ".my-bg-image-el" };
 const NotesList = ({ notes }) => {
   const [notesToDisplay, setNotesToDisplay] = useState(notes);
 
@@ -16,6 +22,20 @@ const NotesList = ({ notes }) => {
   return (
     <NotesListContent>
       {notesToDisplay && notesToDisplay.map(note => <NoteCard key={note._id} data={note} />)}
+      {notesToDisplay && (
+        <Masonry
+          className={"my-gallery-class"} // default ''
+          elementType={"div"} // default 'div'
+          options={masonryOptions} // default {}
+          disableImagesLoaded={false} // default false
+          updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+          imagesLoadedOptions={imagesLoadedOptions} // default {}
+        >
+          {notesToDisplay.map(note => (
+            <NoteCard key={note._id} data={note} />
+          ))}
+        </Masonry>
+      )}
 
       {!notes && <h4>No notes :(</h4>}
     </NotesListContent>
